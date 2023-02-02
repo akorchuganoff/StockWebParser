b82a8247bb0432f361d9936c8af23caeee3f4eb6
Alexandr Korchuganov <akorchuganoff@yandex.ru>
2023-02-02 21:03:13 +0300

    Make a stats command handler

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
+import json
+
 import telebot
 from flask import Flask
 from flask import request, abort
+import requests
 
 import config
-import requests
+from parser import get_stats
 
 # Создание приложения фласк и бота по токену
 app = Flask(__name__)
 bot = telebot.TeleBot(config.token)
 
-## Чтобы подвязать вебхуки - сначала удалите их из бота (если уже были) и установите новые
-# bot.remove_webhook()
-# bot.set_webhook("https://6723-95-64-138-66.eu.ngrok.io/")
 
-#Вьюшка, отвечающая за получение вебхуков от телеграма
+# Чтобы подвязать вебхуки - сначала удалите их из бота (если уже были) и установите новые
+bot.remove_webhook()
+bot.set_webhook("https://0461-95-64-138-66.eu.ngrok.io/")
+
+# Вьюшка, отвечающая за получение вебхуков от телеграма
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.headers.get('content-type') == 'application/json':
@@ -27,8 +31,24 @@ def index():
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '')
+    text = """Привет,\nЯ бот помошник на рынке акций. Я умею находить цену акций, а также читать новости о компании.
+Чтобы добавить бумагу в портфель напиши /add <тикер на бирже>
+Чтобы получить полную информацию - напиши /stats <ticker>"""
+    bot.send_message(message.chat.id, text)
 
+
+@bot.message_handler(commands=['add'])
+def add(message):
+    pass
+
+
+@bot.message_handler(commands=['stats'])
+def stats(message):
+    ticker = message.text.split(maxsplit=1)[1]  # В переменной будет всё,что идёт после /stats
+    bot.send_message(message.chat.id, f'Ticker = {ticker}')
+    answer = get_stats(ticker)
+    text = f"```{json.dumps(answer)}```"
+    bot.send_message(message.chat.id, text)
 
 
 if __name__ == "__main__":
